1d09afd9f9f93b231b9ed42cefc2e0ccc9426719
Marius Delvigne <marius.delvigne@gmail.com>
2024-12-23 18:37:11 +0100

    add search fragment

---
+++ app/src/main/java/com/school/behealth/search/SearchFragment.kt
@@ -0,0 +1,25 @@
+package com.school.behealth.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.school.behealth.R
+import com.school.behealth.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+}
