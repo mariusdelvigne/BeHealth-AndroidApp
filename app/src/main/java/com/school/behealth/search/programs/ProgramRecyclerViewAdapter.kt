7bf1209681d0f07c5ec0d53a074a9d732b4e14c4
Malbecq Nathan <la227315@student.helha.be>
2024-12-27 16:26:26 +0100

    Add description on card click for programs

--- app/src/main/java/com/school/behealth/search/programs/ProgramRecyclerViewAdapter.kt
+++ app/src/main/java/com/school/behealth/search/programs/ProgramRecyclerViewAdapter.kt
@@ -2,13 +2,14 @@ package com.school.behealth.search.programs
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.school.behealth.databinding.FragmentProgramItemBinding
 import com.school.behealth.search.programs.dtos.Program
 
 class ProgramRecyclerViewAdapter(
-    private val values: List<Program>
+    private val values: List<Program>,
 ) : RecyclerView.Adapter<ProgramRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,6 +28,15 @@ class ProgramRecyclerViewAdapter(
         val program = values[position]
         holder.tvTitle.text = program.title
         holder.tvCreatorName.text = program.creatorName
+        holder.tvDescription.text = program.description
+
+        holder.itemView.setOnClickListener {
+            if (holder.tvDescription.visibility == View.GONE) {
+                holder.tvDescription.visibility = View.VISIBLE
+            } else {
+                holder.tvDescription.visibility = View.GONE
+            }
+        }
     }
 
     override fun getItemCount(): Int = values.size
@@ -35,5 +45,6 @@ class ProgramRecyclerViewAdapter(
         RecyclerView.ViewHolder(binding.root) {
         val tvTitle: TextView = binding.tvFragmentProgramItemTitle
         val tvCreatorName: TextView = binding.tvFragmentProgramItemCreatorName
+        val tvDescription: TextView = binding.tvFragmentProgramItemDescription
     }
 }
